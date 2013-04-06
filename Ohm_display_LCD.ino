90bb9fff627dc44cf781269f0300dccf8ffe3b30
sigh8176 <simonchensigh@gmail.com>
2013-04-06 14:10:39 +0800

    Correct Version.
    Display the value of pot on LCD.

--- Ohm_display_LCD.ino
+++ Ohm_display_LCD.ino
@@ -1,19 +1,3 @@
-/* Lab9 - 在 2x16 LCD 上顯示 "Hello World" 訊息  
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * 10K Potentiometer:
- * ends to +5V and ground
- * wiper to LCD Vo pin (pin 3)
- 
- This example code is in the public domain.
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
 // 引用 LiquidCrystal Library
 #include <LiquidCrystal.h>
 
@@ -21,8 +5,7 @@
 //                 LCD 接腳:  rs, enable, d4, d5, d6, d7  
 //      對應到 Arduino 接腳:  12,     11,  5,  4,  3,  2
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-String real = "";
-//char* comdata = "0";
+String MAX = "";
 String comdata="";
 void setup() {
   int loop_x = 1;
@@ -30,20 +13,15 @@ void setup() {
   // 設定 LCD 的行列數目 (2 x 16)
   lcd.begin(16, 2);
   Serial.println("Define the Maximum value of Potentiometer :");
-  real = Maximum_Set_up(loop_x , comdata); 
+  MAX = Maximum_Set_up(loop_x , comdata); 
   lcd.print("Potentiometer");
 }
 void loop() {
-  /*int GG = 0;
-  // 將游標設到 column 0, line 1
-  // (注意: line 1 是第二行(row)，因為是從 0 開始數起):
-  char* test_real[real.length()];
-  real.toCharArray(test_real,real.length());*/
-  long GG =real.toInt();
-  Serial.print(GG);
+  long M2R =MAX.toInt();
+  Serial.print(M2R);
   float sensorValue = analogRead(A0);
   float Display_ohm;
-  long Resistance = GG;
+  long Resistance = M2R;
   lcd.setCursor(0, 1);
   Display_ohm = Get_ohm(sensorValue,Resistance);
   lcd.print(Display_ohm);
